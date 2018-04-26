28e871d14e8c4e1cbff3d226ba978abc6ba5cbe9
Manny <coachmannytw@gmail.com>
2018-04-26 12:27:16 -0600

    Training app with login and profile

---
+++ src/Pages/Getplayer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import Findplayer from '../queries/Findplayer'
+
+
+
+
+
+class Getplayer extends Component {
+
+
+    render() {
+    console.log("Get Player:", this.props)
+        return (
+            <div>
+                <h1>This is from Get Player</h1>
+
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default graphql(Findplayer, {
+        options: {
+            fetchPolicy: 'cache-and-network'
+        },
+        props: props => ({
+            players: props.data.getPlayer ? props.data.getPlayer.player : []
+        })
+    }
+)(Getplayer)
---
+++ src/queries/Findplayer.js
@@ -0,0 +1,14 @@
+import graphql from 'graphql-tag'
+
+export default graphql`query listPlayers {
+    listPlayers {
+        items{
+            firstName
+            lastName
+            position
+            gradClass
+            maxBench
+            maxSquat
+        }
+    }
+}`
